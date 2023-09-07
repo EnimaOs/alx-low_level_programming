430a47fdc71089916a3726d26f5d191a9769c0a5
EnimaOs <ing.devov@gmail.com>
2023-09-08 00:28:01 +0100

    update with betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
 #include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * print and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-    write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 		 sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-         return (1);
+	return (1);
 }
