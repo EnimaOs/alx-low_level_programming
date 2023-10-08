6fc2721121909e3c850612bc4bc7eb0453e9eb3f
EnimaOs <ing.devov@gmail.com>
2023-10-08 10:40:21 +0100

    up

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,34 +1,55 @@
 #include "main.h"
-/**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+int isNumeric(const char *str)
+{
+	while (*str)
+	{
+		if (!isdigit(*str))
+		{
+			return (0);
+		}
+		str++;
+	}
+	return (1);
+}
+
+int multiply(int num1, int num2)
+{
+	return (num1 * num2);
+}
+
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int i, j;
-
 	if (argc != 3)
-	{printf("Error\n");
-	exit(98);
-	}
-	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
-		}
+		printf("Error\n");
+		return (98);
 	}
 
-	mul = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", mul);
+	int num1, num2;
+
+	if (!isNumeric(argv[1]) || !isNumeric(argv[2]))
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	if (num1 <= 0 || num2 <= 0)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	int result = multiply(num1, num2);
+
+	printf("%d\n", result);
 
 	return (0);
-
 }
