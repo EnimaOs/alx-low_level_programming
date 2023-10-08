d02320f19faffbaa137aed3d9306dc97e6c5ff8f
EnimaOs <ing.devov@gmail.com>
2023-10-08 12:23:55 +0100

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -15,28 +15,19 @@ int larger_num(char *n1, char *n2)
 
 	l1 = strlen(n1);
 	l2 = strlen(n2);
-	/*missing cases like 001 19*/
+
 	if (l1 > l2)
 		return (1);
 	else if (l1 < l2)
 		return (2);
-	else if ((*n1 == '\0') && (*n2 == '\0'))
-	{
-		return (1);
-	}
-	else if ((*n1 != '\0') && (*n2 != '\0'))
-	{
-		if (*n1 >= *n2)
-			return (1);
-		if (*n1 < *n2)
-			return (2);
-	}
-	return ((larger_num(n1++, n2++)));
+	else
+		return (strcmp(n1, n2) >= 0 ? 1 : 2);
 }
+
 /**
  * is_number - is it number
  * @n: n1
- * Return: 1 ture or 0 false
+ * Return: 1 true or 0 false
  */
 int is_number(char *n)
 {
@@ -48,44 +39,77 @@ int is_number(char *n)
 	}
 	return (1);
 }
+
 /**
- * mul_number - get larger num
- * @n1: n1
- * @n2: n2
- * Return: larger index.
+ * mul_number - multiply two numbers as strings
+ * @n1: first number as string
+ * @n2: second number as string
+ * Return: result as a dynamically allocated string
  */
 char *mul_number(char *n1, char *n2)
 {
-	char **v;
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
+	int lenResult = len1 + len2;
+	int *result = calloc(lenResult, sizeof(int));
 
-	(void) v;
-	(void) n1;
-	(void) n2;
-	return (NULL);
+	if (result == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (int i = len1 - 1; i >= 0; i--)
+	{
+		for (int j = len2 - 1; j >= 0; j--)
+		{
+			int digit1 = n1[i] - '0';
+			int digit2 = n2[j] - '0';
+			int product = digit1 * digit2;
+
+			int sum = product + result[i + j + 1];
+
+			result[i + j + 1] = sum % 10;
+			result[i + j] += sum / 10;
+		}
+	}
+
+	char *resultStr = malloc(lenResult + 1);
+
+	if (resultStr == NULL)
+	{
+		free(result);
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (int i = 0; i < lenResult; i++)
+	{
+		resultStr[i] = result[i] + '0';
+	}
+	resultStr[lenResult] = '\0';
+	free(result);
+	return (resultStr);
 }
-/**
- * main - multiplies two numbers "arguments"
- * @argc: argument count
- * @argv: argument vector
- * Return: (0) success or (1) faild
- */
+
 int main(int argc, char *argv[])
 {
 	int larger, isNumbers;
 	char *total;
 
-	(void) total;
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	isNumbers = is_number(argv[1]) +  is_number(argv[1]);
-	if (isNumbers != 2)
+
+	isNumbers = is_number(argv[1]) && is_number(argv[2]);
+	if (!isNumbers)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
 	larger = larger_num(argv[1], argv[2]);
 
 	if (larger == 1)
@@ -93,6 +117,9 @@ int main(int argc, char *argv[])
 	else
 		total = mul_number(argv[2], argv[1]);
 
-	printf("%lu\n", (atol(argv[1]) * atol(argv[2])));
+	printf("%s\n", total);
+
+	free(total);
+
 	return (0);
 }
