d97f6d0c5fa481ed19a966abbf520ed406f6e2eb
EnimaOs <ing.devov@gmail.com>
2023-10-27 03:51:32 +0100

    0x14 bit_manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianness.
+ * Return: 0 or 1
+ */
+
+int get_endianness(void)
+{
+	unsigned int x = 1;
+	char *c;
+
+	c = (char *) &x;
+	return ((int) *c);
+}
