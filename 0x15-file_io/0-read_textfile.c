05eb9444c31c7731319b1c64959714176b63d29f
EnimaOs <ing.devov@gmail.com>
2023-10-31 01:20:56 +0100

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#define BUF_SIZE 1024
 /**
  * read_textfile - eads a text file and prints it to the POSIX standard output.
  * @filename: filename
@@ -8,38 +7,19 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-if (filename == NULL)
-{
-return (0);
-}
+	int file_descriptor;
+	char *buf;
 
-FILE *file = fopen(filename, "r");
-if (file == NULL)
-{
-return (0);
-}
-
-char buffer[BUF_SIZE];
-ssize_t total = 0;
-size_t bytes_to_read = letters;
-ssize_t bytes_read;
-
-while (bytes_to_read > 0 && (bytes_read =
-fread(buffer, 1, BUF_SIZE, file)) > 0)
-{
-ssize_t bytes_written = write(STDOUT_FILENO, buffer,
-(bytes_read < bytes_to_read) ? bytes_read : bytes_to_read);
-
-if (bytes_written <= 0 || bytes_written != bytes_read)
-{
-fclose(file);
-return (0);
-}
-
-bytes_to_read -= bytes_read;
-total += bytes_read;
-}
-
-fclose(file);
-return (total);
+	if (filename == NULL)
+		return (0);
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	letters = read(file_descriptor, buf, letters);
+	if ((size_t) write(STDOUT_FILENO, buf, letters) != letters)
+		return (0);
+	close(file_descriptor);
+	free(buf);
+	return (letters);
 }
